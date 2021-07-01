2f5d4c0cc7b99dd9d2b3c574f60d6ad54b5a2892
katrinmasson <72865322+katrinmasson@users.noreply.github.com>
2021-07-01 14:58:07 +0200

    Update leicht.ts

--- Steckbrief/EndabgabeTTT/leicht.ts
+++ Steckbrief/EndabgabeTTT/leicht.ts
@@ -13,10 +13,11 @@ namespace ttt {
     let computerWin: number = 0;
     let nrSpielrunden: number = 0; //Anfangszahl der Spielrunden
     let nrMaxSielrunden: number = 3; //Maximal Anzahl der Spielrunden
-    let cellArray: HTMLDivElement [] = []; //Die einzelnen Zellen 
+    let cellArray: HTMLDivElement[] = []; //Die einzelnen Zellen 
+
 
     //EventListener ButtonLeicht
-    buttonLeicht.addEventListener("click", function(): void {
+    buttonLeicht.addEventListener("click", function (): void {
         let buttonsAll: HTMLElement = document.getElementById("levelButton");
         let board: HTMLElement = document.getElementById("board");
         let middle: HTMLElement = document.getElementById("containerMittel");
@@ -32,49 +33,59 @@ namespace ttt {
             containerLeicht.appendChild(cell);
             cellArray.push(cell);
             //Computer soll beginnen 
-            gameComputer();
             //Eventlistener für einzelne Cells zum Ausfüllen Human & Computer
-            cell.addEventListener("click", function(): void {
-            if (game == true && cell.innerHTML == "") {
-            cell.innerHTML = player;
-            game = false;
-            console.log(cellArray);
-            gameComputer();
-            }});
+            cell.addEventListener("click", function (): void {
+                if (game == true && cell.innerHTML == "") {
+                    cell.innerHTML = player;
+                    game = false;
+                    console.log(cellArray);
+                    gameComputer();
+                }
+            });
         }
-});
+        gameComputer();
+        checkWin();
+    });
 
-    function gameComputer (): void {
-        console.log("computer");
+    function gameComputer(): void {
+        let fieldsEmpty: boolean = false;
+        for (let i: number = 0; i < cellArray.length; i++) {
+            if (cellArray[i].innerHTML == "") {
+                fieldsEmpty = true;
+            }
+        }
         let x: number = Math.floor(Math.random() * 9);
-        if (game == false && cellArray[x].innerHTML == "") {
-            cellArray[x].innerHTML = computer;
-            game = true;
-            console.log(x); 
-    } else {
-        return;
+        if (fieldsEmpty) {
+            if (game == false && cellArray[x].innerHTML == "") {
+
+                cellArray[x].innerHTML = computer;
+                game = true;
+                console.log(x);
+            }
+            if (game == false && cellArray[x].innerHTML != "") {
+                gameComputer();
+            }
+        }
+     
     }
-}
+    //if (game == false && cellArray[x].innerHTML == "") {
 
     //Gewinnmölichkeiten 
-    function checkWin (): void {
-        let winningCombi: any = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
-        for (let i: number = 0; i < winningCombi.length; i++) {
-        let [a, b, c] = winningCombi[i];
-        if (boardValue[a] && boardValue [b] === boardValue[b] && boardValue[c]){
-            return boardValue[a];
-        } else {
-            return null;
-    }
-        console.log(winningCombi);
-}}
+    function checkWin(): void {
+        let winCombi = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+        for (let i: number = 0; i < winCombi.length; i++) {
+            let [a, b, c] = winCombi[i];
+            if (boardValue[a] && boardValue[b] === boardValue[b] && boardValue[c]) {
+                return boardValue[a];
+            } else {
+                return null;
+            }
+        }
+    } console.log(checkWin);
 
-
-    checkWin();
-    
 
 } //Ende namespace
